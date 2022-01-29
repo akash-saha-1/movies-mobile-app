cece919b5daad25d74c2f3246a65862ff8a29cf6
akash-saha-dev <aakashsaha5thjuly@gmail.com>
2022-01-30 01:51:25 +0530

    local demo video implemented

--- components/VideoScreen.js
+++ components/VideoScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import VideoPlayer from 'react-native-video-controls';
+const demovideo = require('./../assets/oceans.mp4');
 
 const VideoScreen = ({onClose}) => {
   return (
     <VideoPlayer
-      source={{uri: 'https://vjs.zencdn.net/v/oceans.mp4'}}
+      //source={{uri: 'https://vjs.zencdn.net/v/oceans.mp4'}}
+      source={demovideo}
       fullscreenOrientation="all"
       onBack={onClose}
       onEnd={onClose}
